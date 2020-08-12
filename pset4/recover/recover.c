403f8439f4f12fb91254962b4d210f64f6db5493
Mark van Bavel <markvbavel@gmail.com>
2020-08-12 12:55:14 +0200

    Improved readability

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -1,19 +1,4 @@
-/*Recover: a program to recover lost JPEG files.
-
-PSEUDOCODE
-Open memory card
-Repeat untill end of card:
-Read 512 bytes into a buffer
-If start of new JPEG
-If first JPEG
-Write 000.jpg .....
-Else
-Close the previous file
-Open new file
-Else
-If already found JPEG
-Keep writing to the jpeg file. It's the second block.
-Close any remaining files */
+/* A program that recovers lost JPEG files by looking for JPEG headers in a .RAW file. */
 
 // Includes
 #include <stdio.h>
@@ -35,6 +20,7 @@ int main(int argc, char *argv[])
 
     // Open memory card
     FILE *file = fopen(argv[1], "r");
+
     // Check for errors while opening the file
     if (file == NULL)
     {
@@ -43,26 +29,26 @@ int main(int argc, char *argv[])
     }
 
     BYTE buffer[512]; // Buffer of 512 bytes
-    FILE *img = NULL; // outfile, set to be blank
-    int jpg_count = 0; // keep track of number of JPEG's
-    char filename[8]; // Name: ###.jpg\0
+    FILE *img = NULL; // Output, set to be blank
+    int jpg_count = 0; // Keep track of number of JPEG's
+    char filename[8]; // Name: "###.jpg\0"
 
     // Read trough the source file, one buffer size at a time, untill there are no more buffer size blocks to read
     while (fread(buffer, sizeof(buffer), 1, file) == 1)
     {
-        // first four bytes. Specified in the short
-        if ((buffer[0] == 0xff) && (buffer[1] == 0xd8) && (buffer[2] == 0xff) && ((buffer [3] & 0xf0) == 0xe0))
+        // first four bytes to recognize a JPEG. Specified in the short
+        if ((buffer[0] == 0xff) && (buffer[1] == 0xd8) && (buffer[2] == 0xff) && ((buffer[3] & 0xf0) == 0xe0))
         {
             // First JPEG
             if (jpg_count == 0)
             {
                 sprintf(filename, "%03i.jpg", jpg_count); // Name for the new file
                 img = fopen(filename, "w"); // open the new file, ready to be written into
-                fwrite(buffer, sizeof(buffer), 1, img); // write the contents of the buffer into the new file
+                fwrite(buffer, sizeof(buffer), 1, img); // write the contents of the buffer into the new file, one buffer at a time
                 jpg_count++; // add one to the jpg_count
             }
 
-            // All other JPEG's
+            // All following JPEG's
             else
             {
                 fclose(img); // close the current open file
@@ -72,10 +58,13 @@ int main(int argc, char *argv[])
                 jpg_count++; // Add to counter
             }
         }
+
+        // If it's not the start of a JPEG, it must be the remaining blocks of the current JPEG
         else if (jpg_count != 0)
         {
             fwrite(buffer, sizeof(buffer), 1, img);
         }
     }
+
     return 0;
 }
